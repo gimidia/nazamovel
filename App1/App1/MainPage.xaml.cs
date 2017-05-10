174ef9636a88ad6045b7f8697850b6ca12dcb6c4
Gilberto Moisés Pereira Soares <Gilberto Moiss Pereira Soares>
2017-05-10 09:48:35 -0300

    Primeiro commit

---
+++ App1/App1/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void MostraLista(Object sender, EventArgs e)
+        {
+            //ListaRefeicoes tela = new ListaRefeicoes();
+            if (edtNomeEmail.Text == "giba" && edtSenha.Text == "123")
+            {
+                await DisplayAlert("Aviso", "Login Aceito", "Ok");
+                await Navigation.PushAsync(new ListaRefeicoes());
+            }
+            else
+            {
+                //if (edtNomeEmail.Text != "giba" & edtSenha.Text != "123")
+                await DisplayAlert("Aviso", "Login não Aceito", "Ok");
+            }
+                       
+                       
+            
+        }
+    }
+}
